f417a020f035c8f9751bee33b625de38591b110c
Aron Bergman <bathingrad@gmail.com>
2018-02-26 13:28:58 +0100

    Fix error with unflushed multi_queries

--- Model/YesNoAnswerPost.php
+++ Model/YesNoAnswerPost.php
@@ -15,7 +15,8 @@ class YesNoAnswerPost extends AnswerPost implements JsonSerializable
 	{
 		$id = $this->uuid2hex($this->generateUUIDv4());
 		$organization_id = $this->base64url2hex($organization_id);
-		if (parent::$db->query("INSERT INTO posts VALUES (UNHEX('$id'), 'answer', UTC_TIMESTAMP()); INSERT INTO answer_posts VALUES (UNHEX('$id'), UNHEX('$organization_id'), 'yes_no', '$question');")) {
+		if (parent::$db->multi_query("INSERT INTO posts VALUES (UNHEX('$id'), 'answer', UTC_TIMESTAMP()); INSERT INTO answer_posts VALUES (UNHEX('$id'), UNHEX('$organization_id'), 'yes_no', '$question');")) {
+			while (parent::$db->more_results()) {parent::$db->next_result();} // Flush the queries
 			// Get the correct UTC_TIMESTAMP() value which was generated in the previous query
 			if ($result = parent::$db->query("SELECT created FROM posts WHERE post_id = UNHEX('$id');")) {
 				$row = $result->fetch_assoc();
@@ -27,6 +28,7 @@ class YesNoAnswerPost extends AnswerPost implements JsonSerializable
 			$this->organization_id = $this->hex2base64url($organization_id);
 		} else {
 			//TODO: Database error
+			echo parent::$db->error;
 		}
 	}
 
@@ -35,8 +37,8 @@ class YesNoAnswerPost extends AnswerPost implements JsonSerializable
 		return array(
 			"post_id" => $this->post_id,
 			"post_type" => $this->post_type,
-			"created" => $this->created,
 			"answer_type" => $this->answer_type,
+			"created" => $this->created,
 			"question" => $this->question,
 			"organization_id" => $this->organization_id
 		);
