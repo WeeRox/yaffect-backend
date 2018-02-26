af8396a429ff16f1b5ec609d93fce8ac4932ccc3
Aron Bergman <bathingrad@gmail.com>
2018-02-26 11:59:14 +0100

    Add query to fetch creation datetime

--- Model/QuestionPost.php
+++ Model/QuestionPost.php
@@ -20,13 +20,18 @@ class QuestionPost extends Post implements JsonSerializable
 		$user_id = $this->base64url2hex($user_id);
 		$organization_id = $this->base64url2hex($organization_id);
 		if (parent::$db->multi_query("INSERT INTO posts VALUES (UNHEX('$id'), 'question', UTC_TIMESTAMP()); INSERT INTO question_posts (post_id, user_id, organization_id, question) VALUES (UNHEX('$id'), UNHEX('$user_id'), UNHEX('$organization_id'), '$question');")) {
+			// Get the correct UTC_TIMESTAMP() value which was generated in the previous query
+			if ($result = parent::$db->query("SELECT created FROM posts WHERE post_id = UNHEX('$id');")) {
+				$row = $result->fetch_assoc();
+				$this->created = $row['created'];
+			}
+
 			$this->post_id = $this->hex2base64url($id);
 			$this->question = $question;
 			$this->organization_id = $this->hex2base64url($organization_id);
 			$this->user_id = $this->hex2base64url($user_id);
 		} else {
 			// TODO: Database error
-			echo parent::$db->error;
 		}
 	}
 
@@ -35,6 +40,7 @@ class QuestionPost extends Post implements JsonSerializable
 		return array(
 			"post_id" => $this->post_id,
 			"post_type" => $this->post_type,
+			"created" => $this->created,
 			"question" => $this->question,
 			"organization_id" => $this->organization_id,
 			"user_id" => $this->user_id
--- Model/YesNoAnswerPost.php
+++ Model/YesNoAnswerPost.php
@@ -5,7 +5,6 @@ use JsonSerializable;
 
 class YesNoAnswerPost extends AnswerPost implements JsonSerializable
 {
-
 	public function __construct()
 	{
 		$this->answer_type = "yes_no";
@@ -16,6 +15,12 @@ class YesNoAnswerPost extends AnswerPost implements JsonSerializable
 		$id = $this->uuid2hex($this->generateUUIDv4());
 		$organization_id = $this->base64url2hex($organization_id);
 		if (parent::$db->query("INSERT INTO posts VALUES (UNHEX('$id'), 'answer', UTC_TIMESTAMP()); INSERT INTO answer_posts VALUES (UNHEX('$id'), UNHEX('$organization_id'), 'yes_no', '$question');")) {
+			// Get the correct UTC_TIMESTAMP() value which was generated in the previous query
+			if ($result = parent::$db->query("SELECT created FROM posts WHERE post_id = UNHEX('$id');")) {
+				$row = $result->fetch_assoc();
+				$this->created = $row['created'];
+			}
+
 			$this->post_id = $this->hex2base64url($id);
 			$this->question = $question;
 			$this->organization_id = $this->hex2base64url($organization_id);
@@ -29,6 +34,7 @@ class YesNoAnswerPost extends AnswerPost implements JsonSerializable
 		return array(
 			"post_id" => $this->post_id,
 			"post_type" => $this->post_type,
+			"created" => $this->created,
 			"answer_type" => $this->answer_type,
 			"question" => $this->question,
 			"organization_id" => $this->organization_id
